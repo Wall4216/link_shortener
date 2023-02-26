0c663e071817eefc77f99464a7dbcd4cca387e93
Wall <ilnazamirhanov61@gmail.com>
2023-02-26 23:21:18 +0300

    add for cdn, but dont working

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
   <head>
     <title></title>
     <!-- ... --->
+      <script src="https://cdn.tailwindcss.com"></script>
    <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/links', [\App\Http\Controllers\LinksController::class,'show'])->name('links.show');
