80ac09f5e828a11ae53789da8ba36f13eb3bd467
Wall <ilnazamirhanov61@gmail.com>
2023-02-26 22:35:37 +0300

    add links request and controller

---
+++ app/Http/Controllers/LinksController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LinksRequest;
+use Illuminate\Http\Request;
+
+class LinksController extends Controller
+{
+    public function show()
+    {
+        return view('links.show');
+    }
+    public function send(LinksRequest $request)
+    {
+        $url = LinksRequest::input('url');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/links', [\App\Http\Controllers\LinksController::class, 'show'])->name('links.show');
+Route::post('links', [\App\Http\Controllers\LinksController::class, 'send'])->name('links.send');
