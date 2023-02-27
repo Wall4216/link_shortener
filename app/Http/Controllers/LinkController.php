f88620c3dda56241210c9412246ed091745e1bdd
Wall <ilnazamirhanov61@gmail.com>
2023-02-27 23:36:48 +0300

    add away method

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -7,6 +7,7 @@ use App\Models\Link;
 use App\Service\LinkService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class LinkController extends Controller
 {
@@ -20,7 +21,7 @@ class LinkController extends Controller
         $url = $request->input('url');
         $description = $request->input('description', null);
 
-        $urlPrefix = $service->get(linkPrefixGenerate());
+        $urlPrefix = $service->linkPrefixGenerate();
         $link = Link::create([
             'source_link' => $url,
             'link_key' => $urlPrefix,
@@ -32,5 +33,15 @@ class LinkController extends Controller
         {
             return back()->with('success', route('links.away', ['prefix'=>$urlPrefix]));
         }
+        return back()->with('errors', 'Не удалось сохранить ссылку');
+    }
+    public function away(string $prefix)
+    {
+        $link = Link::where(['link_key' => $prefix])->first();
+        if($link)
+        {
+            return redirect()->away($link->source_link);
+        }
+        throw new NotFoundHttpException('Prefix not found');
     }
 }
