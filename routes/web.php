d06a5481cfd5485e806ce4967bd70738723f92f6
Wall1642 <92209468+Wall4216@users.noreply.github.com>
2023-05-03 23:59:01 +0300

    Update web.php
    Delete routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/links', [\App\Http\Controllers\LinkController::class,'show'])->name('links.show');
-Route::post('/links', [\App\Http\Controllers\LinkController::class,'send'])->name('links.send');
-Route::get('/links/{prefix}', [\App\Http\Controllers\LinkController::class, 'away'])->name('links.away')->where('prefix', '\w+');
